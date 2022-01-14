6b9a98c11409a6a34f635516367be30a48cc634e
ElHurta <juan.david.hurtado08@gmail.com>
2022-01-14 14:26:29 -0500

    First Commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,14 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Seo from "../components/Seo"
 
 const SecondPage = () => (
-  <Layout>
-    <Seo title="Page two" />
+  <React.Fragment>
+    <Seo title="Page Two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  </React.Fragment>
 )
 
 export default SecondPage
