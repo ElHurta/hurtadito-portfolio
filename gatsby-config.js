e19e71c8ff4f914ca84899c3d2f8364b35e7cc7e
ElHurta <juan.david.hurtado08@gmail.com>
2022-01-15 11:51:52 -0500

    Working on Hero Section

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,16 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/H.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /svg/
+        }
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import Star from '../svg/star.svg'
+import StarBase from '../svg/star-base.svg'
+import { Image } from './Image'
+
+import '../sass/hero.sass'
 
 function Hero() {
     return (
         <section>
+            <div className='gradial-div' />
+            <StarBase />
+            <Star >
+                <Image />
+            </Star>
             
+            <div className='welcome-hero'>
+                <h1>Hi! Welcome To <br />
+                    <span> My Portfolio </span> 
+                </h1>
+                <p>The Name is Juan Hurtado, btw</p>
+            </div>
+
+            {/* Call to action */}
+
         </section>
     )
 }
